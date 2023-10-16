02388ec79276ab3e28218856e6561be00e9af00a
icesnowspirit <147246697+icesnowspirit@users.noreply.github.com>
2023-10-16 21:49:12 +0800

    Update macros2.rs

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     my_macro!();
@@ -13,4 +12,7 @@ macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
+}
+fn main() {
+    my_macro!();
 }
